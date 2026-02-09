5d5fda02c569d623610737654dad34fb036a67b0
Meraxesfire <isabelcaromorillo@gmail.com>
2026-02-09 19:52:09 +0100

    ajuste de flexbox. actualizacion de fuentes. refino cabecera.Añadida GRILLA para catalogo de productos.fijada altura de form

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ console.log("¡Script cargado!");
 
 // Animamos todo lo que tenga la clase .parallax-img
 gsap.to(".parallax-img", {
-    y: 80, // Se desplazan 400px hacia abajo
+    y: 210, // Se desplazan 400px hacia abajo
     ease: "none",
     scrollTrigger: {
         trigger: ".parallax-container",
         start: "top top", 
-        end: "+=200", // El efecto dura 1500px de scroll
+        end: "+=40", // El efecto dura 1500px de scroll
         scrub: 3,
         pin: true,    // Mantiene el contenedor fijo
         markers: true
